e44d6a02f1975a0fc4f4c7ff1991455ac6ab7487
刘跃灿 <995611928@qq.com>
2019-11-08 16:32:53 +0800

    综合项目提交 -1：客户端本地存储

---
+++ ClientStorage/Controllers/WebStorageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClientStorage.Controllers
+{
+    public class WebStorageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
