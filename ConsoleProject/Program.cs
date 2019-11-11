89c787bf64ba3497a36a94b70ecdab34f67b6f4c
刘跃灿 <995611928@qq.com>
2019-11-11 16:32:06 +0800

    添加控制台应用程序--C#7.0使用元组返回多个值

---
+++ ConsoleProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace ConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BackValueTuple.GetPerInfo();               //测试C#7.0元组
+
+            Console.WriteLine("***************测试结束***************");
+            Console.ReadKey();
+        }       
+    }
+}
---
+++ ConsoleProject/库文件/BackValueTuple.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleProject
+{
+    /// <summary>
+    /// 测试C#7.0中函数返回多个值的问题
+    /// </summary>
+    static class BackValueTuple
+    {
+        public static void GetPerInfo()
+        {
+            var perinfo = PerInfo();
+            string name = perinfo.Item1;
+            int age = perinfo.Item2;
+
+            Console.WriteLine($"Hello {name}, 年龄是：{age}");
+        }
+        public static (string ,int) PerInfo()
+        {
+            string name = "gamecc666";
+            int age = 25;
+
+            return (name,age);
+        }
+    }
+}
