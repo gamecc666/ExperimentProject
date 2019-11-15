2be64e6517fa34f4baab8db8bcc75cbe5d94dec3
刘跃灿 <995611928@qq.com>
2019-11-15 15:33:07 +0800

    一个表单多个提交按钮

---
+++ 一个表单多个提交按钮/Controllers/EntranceController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace 一个表单多个提交按钮.Controllers
+{
+    public class EntranceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        ///前台提交数据函数处理
+        [HttpPost]
+        public IActionResult DealData()
+        {
+            FormCollection col = (FormCollection)Request.Form;
+            string name = col["username"];
+            string age = col["age"].ToString();
+
+            Console.WriteLine("***************表单提交来的数据：");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SaveData()
+        {
+            FormCollection col = (FormCollection)Request.Form;
+            string name = col["username"];
+            string age = col["age"].ToString();
+
+            Console.WriteLine("***************表单提交来的数据：");
+            return View();
+        }
+    }
+}
---
+++ 一个表单多个提交按钮/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace 一个表单多个提交按钮.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
