f89d61183e96bf4423c6e84b71281ac6156d534e
刘跃灿 <995611928@qq.com>
2019-11-19 19:11:51 +0800

    两个frame之间的数据传递

---
+++ 两个Frame之间的通信/Controllers/EntranceController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace 两个Frame之间的通信.Controllers
+{
+    public class EntranceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult FirstFrame()
+        {
+            return View();
+        }
+        public IActionResult SecondFrame()
+        {
+            return View();
+        }
+    }
+}
---
+++ 两个Frame之间的通信/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace 两个Frame之间的通信.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
