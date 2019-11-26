473699c97af6f853010bcd33f04b3e1711918f46
刘跃灿 <995611928@qq.com>
2019-11-26 10:25:04 +0800

    前台调用后台方法扩展

---
+++ 前台调用后台方法扩展/Controllers/EntranceController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace 前台调用后台方法扩展.Controllers
+{
+    public class EntranceController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.classInstance = this;
+            return View();
+        }        
+
+        public string GetUserName(int num)
+        {
+            return "gameccc666";
+        }
+    }
+}
---
+++ 前台调用后台方法扩展/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace 前台调用后台方法扩展.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
