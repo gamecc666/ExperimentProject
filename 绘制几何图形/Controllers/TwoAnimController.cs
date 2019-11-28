2eee909705f7aba8df6b3432cb981c33b2b7a977
刘跃灿 <995611928@qq.com>
2019-11-28 09:30:45 +0800

    使用two.js/h5 Canvas画布实现简单示意图

---
+++ 绘制几何图形/Controllers/TwoAnimController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace 绘制几何图形.Controllers
+{
+    public class TwoAnimController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 绘制几何图形/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace 绘制几何图形.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
