aee865096a65d9e505f0f2780e4c859b1975ec80
刘跃灿 <995611928@qq.com>
2019-12-13 18:35:33 +0800

    LinqToXML删除文件

--- LinqToXML从创建到读取/Program.cs
+++ LinqToXML从创建到读取/Program.cs
@@ -8,12 +8,16 @@ namespace LinqToXML从创建到读取
     {
         static void Main(string[] args)
         {
-            /*  创建XML文件
-                var msg= BuildXML.BuilderXMLFile();
-                Console.WriteLine($"信息： {msg}");
-            */
-            string path = "../../../TestXml/gameccinfo.xml";
-            var msg= ReadXML.ReadXMLFile(path);
+            //创建XML文件
+            //var msg = BuildXML.BuilderXMLFile();
+            //Console.WriteLine($"信息： {msg}");
+            //筛选文件
+            //string path = "../../../TestXml/gameccinfo.xml";
+            //var msg = ReadXML.ReadXMLFile(path);
+            //Console.WriteLine($"信息：{msg}");
+            //删除文件
+            string path = "../../../TestXML/gameccinfo.xml";
+            var msg = DeleteXML.DeleteXMLFile(path);
             Console.WriteLine($"信息：{msg}");
         }
     }
---
+++ LinqToXML从创建到读取/res/DeleteXML.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LinqToXML从创建到读取.res
+{
+    static class DeleteXML
+    {
+        public static string DeleteXMLFile(string path)
+        {
+            bool statusCode = true;
+            string errorMsg = null;
+            try
+            {
+                //删除给定目录下的所有文件和文件夹
+                //DirectoryInfo dir = new DirectoryInfo(path);
+                //FileSystemInfo[] fileinfo = dir.GetFileSystemInfos();  //返回目录中所有文件和子目录
+                //foreach (FileSystemInfo i in fileinfo)
+                //{
+                //    if (i is DirectoryInfo)            //判断是否文件夹
+                //    {
+                //        DirectoryInfo subdir = new DirectoryInfo(i.FullName);
+                //        subdir.Delete(true);          //删除子目录和文件
+                //    }
+                //    else
+                //    {
+                //        //如果 使用了 streamreader 在删除前 必须先关闭流 ，否则无法删除 sr.close();
+                //        File.Delete(i.FullName);      //删除指定文件
+                //    }
+                //}
+                if(File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+            }
+            catch(Exception errorinfo)
+            {
+                statusCode = false;
+                errorMsg = errorinfo.ToString();
+            }
+            return statusCode ? "删除成功!" : ($"删除失败，失败信息：{errorMsg}");
+        }
+    }
+}
