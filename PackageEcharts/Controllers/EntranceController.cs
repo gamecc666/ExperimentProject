43d4e4e891d42fa3a7292bdfcf955660ec175ca1
刘跃灿 <995611928@qq.com>
2019-12-24 14:59:42 +0800

    字符串的包含&Echarts的使用

---
+++ PackageEcharts/Controllers/EntranceController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PackageEcharts.Controllers
+{
+    public class EntranceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //按钮响应事件
+        public IActionResult LineChartBtnEvent()
+        {
+            string[] xdatatype = new[] {"Mon","Tue","Wed","Thu","Fri","Sat","Sun" };
+            int[] xdata = new[] {120,200,150,80,70,110,65 };
+
+            return Json(new { xdatatype,xdata});
+        }
+        public IActionResult BarChartBtnEvent()
+        {
+            string[] xdatatype = new[] { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+            int[] xdata = new[] { 120, 200, 150, 80, 70, 110, 65 };
+
+            return Json(new { xdatatype, xdata });
+        }
+        public IActionResult AreaChartBtnEvent()
+        {
+            string[] xdatatype = new[] { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+            int[] xdata = new[] { 120, 200, 150, 80, 70, 110, 65 };
+
+            return Json(new { xdatatype, xdata });
+        }
+        public IActionResult BreadChartBtnEvent()
+        {
+            string[] xdatatype = new[] { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+            int[] xdata = new[] { 120, 200, 150, 80, 70, 110, 65 };
+
+            return Json(new { xdatatype, xdata });
+        }
+    }
+}
---
+++ PackageEcharts/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PackageEcharts.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
