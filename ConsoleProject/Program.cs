8971161beb039c520666d1560bd0941ad680a229
刘跃灿 <995611928@qq.com>
2020-01-11 13:27:45 +0800

    数据格式化提到函数库中

--- ConsoleProject/Program.cs
+++ ConsoleProject/Program.cs
@@ -12,18 +12,14 @@ namespace ConsoleProject
 
             //C#7.0元组，返回多个值
             //BackValueTuple.GetPerInfo();
-            //设置日期格式
-            //Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd hh:mm"));
+            //格式化数据、包含保留小数
+            DataFormat.FormatInfo();
             //测试循环索引
             //Recyle.TestCycle();
             //测试父字符串包含子字符串
             //StringContains.IsStringHasSubstring();
             //string path = @"D:\ZYAPPXmlData\BackData";
-            //GetFileNames.GetFilesName(path);
-            double a = 5.35;
-            double b = 6.354;
-            var res = a / b;
-            Console.WriteLine($"输出运算结果：{res.ToString("#0.0000")}");
+            //GetFileNames.GetFilesName(path);            
             Console.WriteLine("*****************************************测试结束**********************************************");
 
             //string[] atr = new string[] { "1"};
