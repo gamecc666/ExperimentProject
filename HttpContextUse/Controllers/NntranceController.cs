400b34600452b6648516ac84b2decc0e63820d15
刘跃灿 <995611928@qq.com>
2020-03-31 11:42:36 +0800

    对HttpContext的使用

---
+++ HttpContextUse/Controllers/NntranceController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpContextUse.Controllers
+{
+    public class NntranceController : Controller
+    {
+        public IHttpContextAccessor _accessor;
+
+        public NntranceController(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+
+        public IActionResult Index()
+        {
+            //设置响应的头成员
+            if (Response.Headers.ContainsKey("Header001"))
+            {
+                Response.Headers.Add("Header001", "1000");
+            }
+            else
+            {
+                Response.Headers["Header001"] = "1000";
+            }
+
+            var hcontext = _accessor.HttpContext;            
+            return View(hcontext);
+        }
+    }
+}
---
+++ HttpContextUse/ReadMe.md
@@ -0,0 +1,2 @@
+#ReadMe --ASP.NetCore中的HttpContext的使用
+详细可参考网址：https://www.cnblogs.com/OpenCoder/p/10342283.html
