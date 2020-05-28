49f08cd97b2b6aac45c4ff7bc872899917af28e5
gamecc666 <995611928@qq.com>
2020-05-28 14:59:40 +0800

    EFCore数据访问&数据的并发处理

---
+++ MSDN数据访问/ContosoUniversity/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        //指定数据库生成值的方式
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
